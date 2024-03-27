2d71e962f564c082d7338be37611897c32c60718
Godwin <godwin.s@basiscloudsolutions.com>
2024-03-27 16:22:50 +0530

    added login api

---
+++ src/controllers/loginController.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  userId: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model("users", userSchema);
+
+const userLoginController = async (req, res) => {
+  try {
+    const { userId, password } = req.body;
+    const user = await User.findOne({ userId });
+    if (!user) {
+      return res.status(401).json({ flag: "error", message: "User not found" });
+    }
+    if (user.password !== password) {
+      return res
+        .status(401)
+        .json({ flag: "error", message: "Invalid password" });
+    }
+    res.status(200).json({ flag: "success" });
+  } catch (error) {
+    res.status(500).json({ flag: "error", error: error.message });
+  }
+};
+
+module.exports = { userLoginController };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const landingPageRouter = require("./routers/landingPageRouter.js");
+const loginRouter = require("./routers/loginRouter.js");
 
 // Define routes
 router.use("/access-approval", landingPageRouter);
+router.use("/login", loginRouter);
 
 // Export the router
 // module.exports = router;
