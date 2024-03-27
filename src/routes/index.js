4b061210d2dcb56361865629c0d296b5d6c5b974
Hariharan <hariharan.j@basiscloudsolutions.com>
2024-03-27 16:54:15 +0530

    conflicts

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,11 +5,8 @@ const loginRouter = require("./routers/loginRouter.js");
 
 // Define routes
 router.use("/access-approval", landingPageRouter);
-<<<<<<< HEAD
 router.use("/driver", driverPageRouter);
-=======
 router.use("/login", loginRouter);
->>>>>>> 2d71e962f564c082d7338be37611897c32c60718
 
 // Export the router
 // module.exports = router;
